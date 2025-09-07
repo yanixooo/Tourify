123ae8488298395f01dda710943eeec8b47c3d95
yanixooo <zengal.lounes@gmail.com>
2025-09-07 18:46:03 +0100

    initial commit

--- app.js
+++ app.js
@@ -41,17 +41,18 @@ app.use(express.json());
 app.use(express.static(`${__dirname}/public`));
 
 app.use((req, res, next) => {
-  // eslint-disable-next-line no-console
-  console.log('Hello from the middlware 🖐️');
-  next();
-});
-
-app.use((req, res, next) => {
   req.requestTime = new Date().toISOString();
   next();
 });
 
 app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
+
+app.all('*', (req, res, next) => {
+  res.status(404).json({
+    status: 'fail',
+    message: `Can't find ${req.originalUrl} on this server!`,
+  });
+});
 
 export default app;
